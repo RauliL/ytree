7ff07baad1742ee7dd909eb8985d2350f084c846
Rauli Laine <rauli.laine@iki.fi>
2021-08-27 17:44:11 +0300

    Use std::string for system calls

--- src/system.cpp
+++ src/system.cpp
@@ -1,68 +1,53 @@
-/***************************************************************************
- *
- * $Header: /usr/local/cvsroot/utils/ytree/system.c,v 1.12 2008/07/26 14:29:30 werner Exp $
- *
- * System Call
- *
- ***************************************************************************/
-
-
 #include "ytree.h"
 
-
-
-
-int SystemCall(char *command_line)
+int SystemCall(const std::string& command_line)
 {
   int result;
 
   endwin();
-  result = SilentSystemCall( command_line );
-
-  (void) GetAvailBytes( &statistic.disk_space );
+  result = SilentSystemCall(command_line);
+  GetAvailBytes(&statistic.disk_space);
   refresh();
-  return( result );
+
+  return result;
 }
 
-
-int QuerySystemCall(char *command_line)
+int QuerySystemCall(const std::string& command_line)
 {
   int result;
 
   endwin();
-  result = SilentSystemCall( command_line );
+  result = SilentSystemCall(command_line);
   HitReturnToContinue();
-  (void) GetAvailBytes( &statistic.disk_space );
+  GetAvailBytes(&statistic.disk_space);
   refresh();
 
-  return( result );
+  return result;
 }
-
-
 
 extern struct itimerval value, ovalue;
 
-int SilentSystemCall(char *command_line)
+int SilentSystemCall(const std::string& command_line)
 {
-  return(SilentSystemCallEx(command_line, true));
+  return SilentSystemCallEx(command_line, true);
 }
 
-int SilentSystemCallEx(char *command_line, bool enable_clock)
+int SilentSystemCallEx(const std::string& command_line, bool enable_clock)
 {
   int result;
 
-  /* Hier ist die einzige Stelle, in der Kommandos aufgerufen werden! */
+  // Hier ist die einzige Stelle, in der Kommandos aufgerufen werden!
+  SuspendClock();
 
-    SuspendClock();
-
-  result = system( command_line );
+  result = std::system(command_line.c_str());
 
   leaveok(stdscr, true);
   curs_set(0);
-  if(enable_clock)
+  if (enable_clock)
+  {
     InitClock();
-  (void) GetAvailBytes( &statistic.disk_space );
-  return( result );
+  }
+  GetAvailBytes(&statistic.disk_space);
+
+  return result;
 }
-
-
